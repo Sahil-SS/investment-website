66d42a34df64430d7cf18472852989d08071ad60
Sahil Sinha <sinhasahil272004@gmail.com>
2025-10-29 12:19:33 +0530

    build auth and paymemnt systems

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,9 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // ✅ disable 'any' rule globally
+    },
   },
 ];
 
